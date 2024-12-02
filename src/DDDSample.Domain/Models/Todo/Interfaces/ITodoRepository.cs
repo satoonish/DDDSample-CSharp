8c09b71e09332a24065a9cdbe658bf049e2f6421
Satsuki <satsuki200214@gmail.com>
2024-12-02 23:50:45 +0900

    [add]Todoのリポジトリを追加
    DBを使わないリポジトリを仮実装しました

---
+++ src/DDDSample.Domain/Models/Todo/Interfaces/ITodoRepository.cs
@@ -0,0 +1,30 @@
+﻿using DDDSample.Domain.Models.Todo.Entities;
+using DDDSample.Domain.Models.Todo.ValueObjects;
+
+namespace DDDSample.Domain.Models.Todo.Interfaces
+{
+    public interface ITodoRepository
+    {
+        /// <summary>
+        /// Todo エンティティを保存します
+        /// </summary>
+        Task Save(TodoEntity entity);
+
+        /// <summary>
+        /// 指定した Todo ID のエンティティを削除します
+        /// </summary>
+        /// <param name="id">Todo の識別 ID</param>
+        Task DeleteByID(TodoID id);
+
+        /// <summary>
+        /// すべての Todo エンティティを取得します
+        /// </summary>
+        Task<List<TodoEntity>> FindAll();
+
+        /// <summary>
+        /// 指定したステータスのすべての Todo エンティティを取得します
+        /// </summary>
+        /// <param name="status">Todo のステータス</param>
+        Task<List<TodoEntity>> FindAllByStatus(TodoStatus status);
+    }
+}
---
+++ src/DDDSample.Infrastructure/Todo/FakeTodoRepository.cs
@@ -0,0 +1,50 @@
+﻿using DDDSample.Domain.Models.Todo.Entities;
+using DDDSample.Domain.Models.Todo.Interfaces;
+using DDDSample.Domain.Models.Todo.ValueObjects;
+
+namespace DDDSample.Infrastructure.Todo
+{
+    /// <summary>
+    /// DB を使わない仮の Todo リポジトリ
+    /// </summary>
+    internal class FakeTodoRepository : ITodoRepository
+    {
+        private List<TodoEntity> _repository = [];
+
+        public Task Save(TodoEntity entity)
+        {
+            _repository.Add(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteByID(TodoID id)
+        {
+            var deleteEntity = _repository.FirstOrDefault(e => e.ID == id);
+            if (deleteEntity != null)
+            {
+                _repository.Remove(deleteEntity);
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task<List<TodoEntity>> FindAll()
+        {
+            return Task.FromResult(_repository);
+        }
+
+        public Task<List<TodoEntity>> FindAllByStatus(TodoStatus status)
+        {
+            var result = new List<TodoEntity>();
+
+            foreach (var entity in _repository)
+            {
+                if (entity.Status == status)
+                {
+                    result.Add(entity);
+                }
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
