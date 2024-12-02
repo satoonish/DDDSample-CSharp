0e82b9b78ecc2874becef8493c1fd7d4a1d423af
Satsuki <satsuki200214@gmail.com>
2024-12-03 00:04:09 +0900

    [add]Todoのファクトリを追加

---
+++ src/DDDSample.Infrastructure/Todo/TodoFactory.cs
@@ -0,0 +1,12 @@
+﻿using DDDSample.Domain.Models.Todo.Interfaces;
+
+namespace DDDSample.Infrastructure.Todo
+{
+    public static class TodoFactory
+    {
+        public static ITodoRepository CreateTodoRepository()
+        {
+            return new FakeTodoRepository();
+        }
+    }
+}
