c4474492f816dd5baff959e07216d4f3b6bb7813
Satsuki <satsuki200214@gmail.com>
2024-12-03 00:41:30 +0900

    [add]Prismを適用しました

--- src/DDDSample.Wpf/App.xaml.cs
+++ src/DDDSample.Wpf/App.xaml.cs
@@ -1,14 +1,25 @@
-﻿using System.Configuration;
+﻿using DDDSample.Wpf.Views;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
 namespace DDDSample.Wpf
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application
-	{
-	}
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            var window = Container.Resolve<MainWindow>();
+            return window;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // DI コンテナに登録
+        }
+    }
 
 }
