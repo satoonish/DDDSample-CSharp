d317546e3b7a310a6ce5e8654d93725fa42e9b52
Satsuki <satsuki200214@gmail.com>
2024-12-02 22:26:25 +0900

    [add]Todoのエンティティを追加

---
+++ src/DDDSample.Domain/Models/Todo/Entities/TodoEntity.cs
@@ -0,0 +1,29 @@
+﻿using DDDSample.Domain.Models.Todo.ValueObjects;
+
+namespace DDDSample.Domain.Models.Todo.Entities
+{
+    public class TodoEntity
+    {
+        public TodoEntity(
+            TodoID id,
+            TodoTitle title,
+            TodoDescription description,
+            TodoDeadline deadline,
+            TodoStatus status)
+        {
+            CreationTime = DateTime.Now;
+            ID = id;
+            Title = title;
+            Description = description;
+            Deadline = deadline;
+            Status = status;
+        }
+
+        public DateTime CreationTime { get; }
+        public TodoID ID { get; }
+        public TodoTitle Title { get; }
+        public TodoDescription Description { get; }
+        public TodoDeadline Deadline { get; }
+        public TodoStatus Status { get; }
+    }
+}
