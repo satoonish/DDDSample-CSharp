3c83f1f56f95321314a13ff9b1df213ca5c1538a
Satsuki <satsuki200214@gmail.com>
2024-12-05 00:15:15 +0900

    [add]Wpfのディレクトリ構造を作成しました

---
+++ src/DDDSample.Wpf/Models/TodoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDSample.Wpf.Models
+{
+    public class TodoModel
+    {
+        public string ID { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Deadline { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
---
+++ src/DDDSample.Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DDDSample.Wpf.Models;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDSample.Wpf.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public ReactiveCollection<TodoModel> TodoModels { get; set; } = [];
+    }
+}
